fce94cd3ea00d29995eb04b9afd601afb799ec5c
Omni <coven3d2015@hotmail.com>
2024-05-05 22:13:46 -0600

    im going insane

--- sigil/src/components/Dialogue/Textbox.jsx
+++ sigil/src/components/Dialogue/Textbox.jsx
@@ -1,12 +1,28 @@
 import '~/styles/Dialogue/Textbox.css'
 import { useTypewriter } from '../../hooks/useTypewriter';
+import { useState } from 'react';
 
 const Textbox = ({text}) => {
 
-    const [displayText, finishText] = useTypewriter(text, 50, () => {console.log("typing finished")});
+    const [currentText, setCurrentText] = useState(text);
+
+    // TODO: hoist all this up to Dialogue, because we need to link the buttons to this shit 
+    // + we need the handlers for sprites etc.
+    const [displayText, finishText] = useTypewriter(currentText, 50, () => {console.log("typing finished"); setLineFinished(true)});
+
+    const [lineFinished, setLineFinished] = useState(false);
+
+    const advanceText = () => {
+        if(lineFinished) {
+            console.log("Please god")
+            setCurrentText("IM GOING INSANE");
+        } else {
+            finishText();
+        }
+    }
 
     return (
-        <div className="Textbox" onClick={finishText}> 
+        <div className="Textbox" onClick={advanceText}> 
             <p>
                 {displayText}
             </p>
