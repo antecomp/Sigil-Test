747652c3ac088c58816c0c4842d6b042f9d37162
Omni <coven3d2015@hotmail.com>
2024-05-09 17:26:28 -0600

    rough setup of toastify

--- sigil/src/App.jsx
+++ sigil/src/App.jsx
@@ -6,18 +6,71 @@ import Modal from "~/components/Modal/Modal.jsx"
 import "react-modal-global/styles/modal.scss" // Imports essential styles for `ModalContainer`.
 import "~/styles/Modal/ModalOverrides.css"
 
+import { ToastContainer, toast, Slide } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '~/styles/extra/Toastify.css'
+
+import svalinn from '~/artwork/usrhead.png'
+
+const TestNotif = ({closeToast, toastProps, title, msg}) => {
+  return (
+    <div className='messageNotif'>
+      <img src={svalinn} alt="" />
+      <div>
+        <h3>{title}</h3>
+        What do you think of the new interface buddy?
+      </div>
+    </div>
+  )
+}
+
+const PlaceHolderNotifProps = {
+  title: "Svalinn",
+  msg: "What do you think of the new interface buddy?"
+}
+
 function App() {
+
+  const notify = () => {
+    /*toast(<TestNotif />, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+      progress: undefined,
+      theme: "dark",
+      transition: Slide,
+    }); */
+    toast(<TestNotif {...PlaceHolderNotifProps} />, {transition: Slide})
+  }
 
   return (
     <>
-    <div className="App" id='window'>
+      <div className="App" id='window'>
 
-    
-      {/* <BattleCon/> */}
 
-      <NSGraph />
-    </div>
-    <ModalContainer controller={Modal}/>
+        {/* <BattleCon/> */}
+
+        <NSGraph />
+        <button onClick={notify}>Notify!</button>
+      </div>
+      <ModalContainer controller={Modal} />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover
+        theme="dark"
+        closeButton={false}
+        transition: Slide
+      />
     </>
   );
 }
